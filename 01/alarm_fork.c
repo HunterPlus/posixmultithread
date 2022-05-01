0b3a13222803cfca3eebb73cd413805ecdafef09
HunterPlus <zlw08140957@gmail.com>
2022-05-01 13:29:19 +0800

    Update alarm_fork.c

--- 01/alarm_fork.c
+++ 01/alarm_fork.c
@@ -11,6 +11,35 @@ int main(int argc, char *argv[])
 	char	message[64];
 	
 	while (1) {
-		
+		printf("Alarm> ");
+		if (fgets(line, sizeof (line), stdin) == NULL)
+			exit(0);
+		if (strlen(line) <= 1)
+			continue;
+		if (sscanf(line, "%d %64[^\n]", &seconds, message) < 2)
+			fprintf(stderr, "Bad command\n");
+		else {
+			pid = fork();
+			if (pid == (pid_t) -1)
+				errno_abort("Fork");
+			if (pid == (pid_t) 0) {
+				/*
+				 * In the child, wait and then print a message
+				 */
+				sleep(seconds);
+				printf("%d %s\n", seconds, message);
+				exit(0);
+			} else {
+				/* 
+				 * In the parent, call waitpid() to collect children
+				 * that have already terminate.
+				 */
+				do {
+					pid = waitpid((pid_t) -1, NULL, WNOHAND);
+					if (pid = (pid_t) -1)
+						errno_abort("wait for child");
+				} while (pid != (pid_t) 0);
+			}
+		}
 	}
 }
